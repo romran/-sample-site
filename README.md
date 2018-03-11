e2dc94ef3123d176aff455dba95bebef1fa16877
romran <flowestinfo@gmail.com>
2018-03-12 00:17:16 +0200

    readme and fixes

---
+++ README.md
@@ -0,0 +1,16 @@
+Sample Site front-end mock up with Gulp. [DEMO](https://github.com/facebookincubator/create-react-app) 
+
+Quick setup:
+
+`npm install` to set up dependencies
+
+`bower install` to install up JS plugins dependencies
+
+`gulp` will open Sample Site in http://localhost:3000/ 
+
+Run gulp tasks to compile vendor.js and vendor.css files if needed. 
+
+ 
+
+ 
+ 
--- assets/js/src/custom.js
+++ assets/js/src/custom.js
@@ -25,15 +25,75 @@ $(document).ready(function () {
         }
     });
 
-    $(window).on('resize', function(){
+    $(window).on('resize', function () {
         var width = $(window).width();
-        if (width<=1024 && width>=767) {
+        if (width <= 1024 && width >= 767) {
             $(".dropdown-content:last").addClass("move");
         }
         else {
-             $(".dropdown-content:last").removeClass("move");
+            $(".dropdown-content:last").removeClass("move");
         }
-     }).trigger('resize');
+    }).trigger('resize');
+
+
+  /*  $("#firstname").on("blur", function () {
+        if ($(this).val().match('^[a-zA-Z]')) {
+        } else {
+            alert("That's not a name");
+        }
+    });
+
+    $("#lastname").on("blur", function () {
+        if ($(this).val().match('^[a-zA-Z]')) {
+        } else {
+            alert("That's not a name");
+        }
+    });*/
+
+    $("#submit").click(function () {
+        if ( $("#lastname").val().match('^[a-zA-Z]')) {
+            $("#lastname").css("border","none")
+        }
+        else {
+            $("#lastname").css("border","1px solid red");
+            alert("Last name consists invalid characters");
+        }
+
+        if ( $("#firstname").val().match('^[a-zA-Z]')) {
+            $("#firstname").css("border","none")
+        }
+        else {
+            $("#firstname").css("border","1px solid red");
+            alert("First name consists invalid characters");
+        }
+
+    });
+
+
+
+    function getIEVersion() {
+        var rv = -1; // Return value assumes failure.
+        if (navigator.appName == 'Microsoft Internet Explorer') {
+            var ua = navigator.userAgent;
+            var re  = new RegExp("MSIE ([0-9]{1,}[\.0-9]{0,})");
+            if (re.test(ua) != null)
+                rv = parseFloat( RegExp.$1 );
+        }
+        return rv;
+    }
+
+    function checkVersion() {
+        var ver = getIEVersion();
+        if ( ver != -1 ) {
+            if (ver <= 10.0) {
+                alert("The Site don't support insecure browsers, please upgrade to Internet Explorer 11 or use another modern browser ");
+            }
+        }
+    }
+    checkVersion();
+
+
+
 
 });
 
