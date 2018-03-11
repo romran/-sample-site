940942c8db1fbdb9b5ce84b80f811d80ea0e8426
romran <flowestinfo@gmail.com>
2018-03-12 01:29:05 +0200

    fixes

--- README.md
+++ README.md
@@ -1,10 +1,12 @@
-Sample Site front-end mock up boilerplate with Gulp. [DEMO](http://htmlpreview.github.io/?https://github.com/romran/sample-site/blob/master/index.html) 
+####Sample Site frontend mock-up boilerplate with Gulp. 
+
+[DEMO](http://htmlpreview.github.io/?https://github.com/romran/sample-site/blob/master/index.html) 
 
 Quick setup:
 
 `npm install` to set up dependencies
 
-`bower install` to install up JS plugins dependencies
+`bower install` to install JS libraries
 
 `gulp` will open Sample Site in http://localhost:3000/ 
 
--- assets/js/src/custom.js
+++ assets/js/src/custom.js
@@ -1,7 +1,6 @@
 $(document).ready(function () {
 
     $(".dropbtn").click(function () {
-        console.log(true);
         $(".dropdown-content").removeClass("show");
         $(".dropbtn").css("color", "white");
         $(this).css("color", "#333333");
@@ -35,66 +34,45 @@ $(document).ready(function () {
         }
     }).trigger('resize');
 
-
-  /*  $("#firstname").on("blur", function () {
-        if ($(this).val().match('^[a-zA-Z]')) {
-        } else {
-            alert("That's not a name");
-        }
-    });
-
-    $("#lastname").on("blur", function () {
-        if ($(this).val().match('^[a-zA-Z]')) {
-        } else {
-            alert("That's not a name");
-        }
-    });*/
-
     $("#submit").click(function () {
-        if ( $("#lastname").val().match('^[a-zA-Z]')) {
-            $("#lastname").css("border","none")
+        if ($("#lastname").val().match('^[a-zA-Z]')) {
+            $("#lastname").css("border", "none")
         }
         else {
-            $("#lastname").css("border","1px solid red");
+            $("#lastname").css("border", "1px solid red");
             alert("Last name consists invalid characters");
         }
 
-        if ( $("#firstname").val().match('^[a-zA-Z]')) {
-            $("#firstname").css("border","none")
+        if ($("#firstname").val().match('^[a-zA-Z]')) {
+            $("#firstname").css("border", "none")
         }
         else {
-            $("#firstname").css("border","1px solid red");
+            $("#firstname").css("border", "1px solid red");
             alert("First name consists invalid characters");
         }
-
     });
-
-
 
     function getIEVersion() {
         var rv = -1; // Return value assumes failure.
         if (navigator.appName == 'Microsoft Internet Explorer') {
             var ua = navigator.userAgent;
-            var re  = new RegExp("MSIE ([0-9]{1,}[\.0-9]{0,})");
+            var re = new RegExp("MSIE ([0-9]{1,}[\.0-9]{0,})");
             if (re.test(ua) != null)
-                rv = parseFloat( RegExp.$1 );
+                rv = parseFloat(RegExp.$1);
         }
         return rv;
     }
 
     function checkVersion() {
         var ver = getIEVersion();
-        if ( ver != -1 ) {
+        if (ver != -1) {
             if (ver <= 10.0) {
                 alert("The Site don't support insecure browsers, please upgrade to Internet Explorer 11 or use another modern browser ");
             }
         }
     }
-    checkVersion();
 
-
-
-
+    checkVersion()
 });
 
 
